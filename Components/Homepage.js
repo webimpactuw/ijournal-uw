b99976de08894cf0569f29e34fd8c18a2965ecf3
Sam Chan <chansam55260@gmail.com>
2026-03-04 01:32:57 -0800

    Fixed branch assets folder and bg image positioning

--- Components/Homepage.js
+++ Components/Homepage.js
@@ -1,7 +1,6 @@
 "use client"
 import Link from "next/link";
 import { motion } from "motion/react";
-import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
 import Image from "next/image"
 import Illustration from "../assets/Logo/landingpage_illustration.png"
@@ -9,7 +8,7 @@ import Illustration from "../assets/Logo/landingpage_illustration.png"
 export default function Homepage() {
     return(
         <div className="w-full">
-            <Image src={Illustration} alt="iJournal landing page background"/>
+            <Image className="absolute" src={Illustration} alt="iJournal landing page background"/>
         </div>
     );
 }
