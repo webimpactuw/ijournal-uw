1bb6350613fd4a588b5d670025921ca18bb330db
kim's mac <kim.kp.pham@gmail.com>
2026-03-08 15:13:17 -0700

    Added Featured Articles Section

--- app/page.js
+++ app/page.js
@@ -1,7 +1,12 @@
 "use client"; 
+import Homepage from "../Components/Homepage";
+import FeaturedArticles from "../Components/FeaturedArticles";
 
 export default function Home() {
   return (
-    <div></div>
+    <div>
+      <Homepage />
+      <FeaturedArticles />
+    </div>
   );
 }
