f5a78079f33edfa12a83e2cbaac2a791ebf7b61a
Sam Chan <chansam55260@gmail.com>
2026-03-07 17:17:35 -0800

    Implemented homepage, added fonts to globals.css

--- Components/Homepage.js
+++ Components/Homepage.js
@@ -8,21 +8,21 @@ import Logo from '../assets/Logo/iJournal_Logo.svg';
 
 export default function Homepage() {
     return(
-        <div className="w-full relative min-h-screen flex justify-start items-start overflow-hidden">
+        <div className="w-full relative min-h-screen flex justify-start items-start overflow-hidden italic font-medium">
             <Image className="absolute z-[-1] object-cover" fill src={Illustration} alt="iJournal landing page background"/>
             
             <div className="absolute w-7/24 top-60 left-255 px-6 py-8">
                 <Image className="" src={Logo} alt="iJournal logo" />
-                <p className="left-10 text-center" >The UW iSchool's first student-led journal</p>
+                <p className="left-10 text-center text-navBar!" >The UW iSchool's first student-led journal</p>
             </div>
 
-            <div className="absolute top-120 left-155 max-w-1/5">
-                <p className="">I saw my life branching out 
-                    before me like the green 
+            <div className="absolute top-120 left-160 max-w-1/4">
+                <p className="">I saw my life branching out<br/>
+                    before me like the green<br/>
                     fig tree in the story.</p>
-                <div className="relative top-10 left-35 text-right">
-                    <p>From the tip of every 
-                        branch, like a fat purple fig,
+                <div className="relative top-5 left-30 text-right">
+                    <p>From the tip of every <br/>
+                        branch, like a fat purple fig, <br/>
                         a wonderful future beckoned.</p>
                     
                     <p>SYLVIA PLATH, THE BELL JAR</p>
