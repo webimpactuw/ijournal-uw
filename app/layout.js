d461c4340499ef7907214fceb1f5119739d6a643
Nathan-123 <nathantadams@hotmail.com>
2026-03-10 22:54:34 -0700

    merged footer

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "../Components/Navbar";
-import Footer from "@/Components/Footer";
+import Footer from "../Components/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,11 +25,8 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <Navbar />
-      <Homepage />
-      <Footer />
-
-        <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
