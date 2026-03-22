7f8cf29d8cc613488bf5bc9ed555874be50714f5
Sam Chan <chansam55260@gmail.com>
2026-03-22 15:39:25 -0700

    Fixed homepage text locations

--- Components/Homepage.js
+++ Components/Homepage.js
@@ -8,27 +8,27 @@ import Logo from '../assets/Logo/iJournal_Logo.svg';
 
 export default function Homepage() {
     return(
-        <div className="w-full relative min-h-screen flex justify-start items-start overflow-hidden italic font-medium">
+        <div className="w-full relative min-h-screen flex justify-start items-start overflow-hidden font-medium">
             <Image className="absolute z-[-1] object-cover" fill src={Illustration} alt="iJournal landing page background"/>
             
-            <div className="absolute w-7/24 top-60 left-255 px-6 py-8">
+            <div className="absolute w-7/24 top-[30%] left-[70%] px-6 py-8">
                 <Image className="" src={Logo} alt="iJournal logo" />
-                <p className="left-10 text-center text-navBar!" >The UW iSchool's first student-led journal</p>
+                <p className="left-10 text-center text-navBar" >The UW iSchool's first student-led journal</p>
             </div>
 
-            <div className="absolute top-120 left-160 max-w-1/4">
+            <div className="absolute top-[65%] left-[42%] max-w-1/4 italic font-bold text-main">
                 <p>
                     I saw my life branching out<br/>
                     before me like the green<br/>
                     fig tree in the story.
                 </p>
-                <div className="relative top-5 left-30 text-right">
+                <div className="relative top-5 left-35 text-right italic">
                     <p>
                         From the tip of every <br/>
                         branch, like a fat purple fig, <br/>
                         a wonderful future beckoned.
                     </p>
-                    <p>SYLVIA PLATH, THE BELL JAR</p>
+                    <p className="font-dm-mono" >SYLVIA PLATH, THE BELL JAR</p>
                 </div>
             </div>
         </div>
