8dd4d0d190e38e0333a29b2e2d2f7e510117da03
Sam Chan <chansam55260@gmail.com>
2026-04-19 21:39:50 -0700

    Created Journals header

--- app/journals/page.js
+++ app/journals/page.js
@@ -1,7 +1,11 @@
 export default function Journals() {
   return (
-    <div>
-      <h1>Journals</h1>
+    <div className="bg-white text-navBar font-garamond text-4xl py-16">
+      <div className="mx-auto w-[min(80vw,1300px)]">
+        <h1 className="">Every school year, the iJournal team works to curate a collection of our favorite articles published throughout the quarter!</h1>
+        <h1 className="mt-15 font-batmip italic text-[45px] border-b border-[#d9ccd8]">Journals</h1>
+      </div>
+      <div className="h-8"></div>
     </div>  
   );
 }
