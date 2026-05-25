fd35680cb4effe39e6b82d7ef55cafec38c4bc64
snareboi <justcube740@gmail.com>
2026-05-24 20:12:15 -0700

    More comment section features
    There is now character size cap for comments. They are also sorted with newest at the top and there is message under the comment header when there are no comments.

--- Components/CommentSection.js
+++ Components/CommentSection.js
@@ -5,15 +5,25 @@ import dbConnect from "@/lib/mongodb";
 export default async function CommentSection({articleInfo}) {
     await dbConnect();
     const articleTitle = articleInfo.title;
-    const comments = await Comment.find({article: articleTitle});
+    const comments = await Comment.find({article: articleTitle}).sort({createdAt: -1});
 
     //TODO sort comments with newest at the top
     return (
         <div className="mx-auto w-[min(92vw,650px)] pb-30">
             <h1 className="text-[#7a1b74] text-3xl"><i>Comments</i></h1>
-            {comments.map((comment) => (
-                <IndividualComment key={comment._id} comment={comment} />
-            ))}
+
+            {(comments.length > 0) && (
+                <>
+                    {comments.map((comment) => (
+                        <IndividualComment key={comment._id} comment={comment} />
+                    ))}
+                </>
+            )}
+
+            {(comments.length == 0) && (
+                <h2 className="mt-1">Nothing to see here yet. Be the first to comment!</h2>
+            )}
+            
         </div>
     );
 
